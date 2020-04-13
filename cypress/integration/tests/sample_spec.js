8df977396f48639ee384386c002354b062bc9abb
sheprnd <sheprnd@gmail.com>
2020-04-13 15:03:43 +0300

    gitignored and removed tests

---
+++ cypress/integration/tests/sample_spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  })
+
+  describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(false)
+    })
+  })
