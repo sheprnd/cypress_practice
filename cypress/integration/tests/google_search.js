f46e4b556709f1646a5df95610d635e95b1a5795
sheprnd <sheprnd@gmail.com>
2020-04-13 16:18:56 +0300

    improve google search test

--- cypress/integration/tests/google_search.js
+++ cypress/integration/tests/google_search.js
@@ -3,7 +3,9 @@
     describe('Make a search', () => {
       it('.should() - find searchable info', () => {
         cy.visit('https://www.google.com/')
-        cy.get('input.gLFyf.gsfi')
-      .type('fake@email.com').should('have.value', 'fake@email.com')
+        cy.get('input[type = "text"]')
+        .type('Мир дикого запада').should('have.value', 'Мир дикого запада')
+        .type('{enter}')
+        cy.get('a[href*=wiki')
     }) 
   })
